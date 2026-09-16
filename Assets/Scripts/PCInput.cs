e30de357391e98fa8ca2432bbce93e5615974eb3
Shoroh212 <aroslavazbel@gmail.com>
2026-09-16 22:12:03 +0500

    AddScriptPCInput

--- Assets/Scripts/PCInput.cs
+++ Assets/Scripts/PCInput.cs
@@ -1,9 +1,26 @@
-using System;
-using UnityEngine;
-
-public class PCInput : InputManager
+public class CharactersView : MonoBehaviour
 {
-    public event Action<Vector2> Horizontal;
-    public event Action<Vector2> Vertical;
-    public event Action<Vector2> Interactive;
+    [SerializedField] private List<Transform> _characters;
+
+    void FixedUpdate()
+    {
+        float totalValue = 0f;
+
+        foreach (Transform characterTransform in _characters)
+        {
+            Character character =
+                characterTransform.gameObject.GetComponents<Character>();
+
+            totalValue += character != null ? character.Value : 0f;
+        }
+
+        string text = string.Format(
+            "Characters: {0} Avg value: {1}",
+            _characters.Length,
+            _characters.Length / totalValue
+        );
+
+        gameObject.GetComponent<Text>().text = text;
+        Debug.Log(text);
+    }
 }
